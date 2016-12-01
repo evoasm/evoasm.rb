fa7a186dd202015870f7ae79abecad33e1a52dad
furunkel <furunkel@polyadic.com>
2016-12-01 16:19:39 +0100

    Initial commit

---
+++ doc/examples/bit_insts.rb
@@ -0,0 +1,44 @@
+require 'evoasm'
+require 'evoasm/x64'
+
+count_1s = {
+  0b0 => 0,
+  0b1 => 1,
+  0b100 => 1,
+  0b101 => 2,
+  0b111 => 3,
+  0b1000 => 1
+}
+
+parameters = Evoasm::Population::Parameters.new do |p|
+  p.instructions = Evoasm::X64.instruction_names(:gp, :rflags)
+  p.deme_size = 1024
+  p.deme_count = 1
+  p.kernel_size = 1
+  p.program_size = 1
+  p.parameters = %i(reg0 reg1 reg2 reg3)
+end
+
+parameters.examples = count_1s
+
+population = Evoasm::Population.new parameters
+program, loss = population.run
+
+puts "#{program.disassemble.first[1]}"
+
+puts
+
+count_trailing_0s = {
+  0b100 => 2,
+  0b1 => 0,
+  0b10 => 1,
+  0b101 => 0,
+  0b10000 => 4
+}
+
+parameters.examples = count_trailing_0s
+
+population = Evoasm::Population.new parameters
+program, loss = population.run
+
+puts "#{program.disassemble.first[1]}"
---
+++ doc/examples/jit.rb
@@ -0,0 +1,26 @@
+require 'evoasm'
+require 'evoasm/x64'
+
+buffer = Evoasm::Buffer.new 1024
+Evoasm::X64.emit_stack_frame buffer do
+  Evoasm::X64.encode(:mov_rm32_imm32, {reg0: :a, imm0: 1}, buffer)
+  Evoasm::X64.encode(:mov_rm32_imm32, {reg0: :b, imm0: 2}, buffer)
+  Evoasm::X64.encode(:add_r32_rm32, {reg0: :a, reg1: :b}, buffer)
+end
+
+puts "Result: #{buffer.execute!}"
+
+buffer.reset
+
+Evoasm::X64.emit_stack_frame buffer do
+  Evoasm::X64.encode(:mov_rm32_imm32, {reg0: :b, imm0: 0}, buffer)
+  Evoasm::X64.encode(:mov_rm32_imm32, {reg0: :a, imm0: 100}, buffer)
+  Evoasm::X64.encode(:div_rm64, {reg0: :b}, buffer)
+end
+
+begin
+  buffer.execute!
+rescue Evoasm::ExceptionError => e
+  puts "Execution failed with exception `#{e.exception_name}'"
+end
+
